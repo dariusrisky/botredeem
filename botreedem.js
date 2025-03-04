5114bea29ffccaae4250b4db1d5ad948d86d4846
risky <riskyramdani8fno26@gmail.com>
2025-03-04 12:38:14 +0700

    beta 0.00.01

--- botreedem.js
+++ botreedem.js
@@ -5,16 +5,34 @@ const fs = require('fs');
 const path = require('path');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
+const TARGET_ADMIN_CHAT_ID = process.env.TARGET_ADMIN_CHAT_ID;
+const ADMIN_IDS = process.env.BROADCAST_ID;
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
 const RANGE = 'Sheet1!A2:c';  
-
 const auth = new google.auth.GoogleAuth({
   keyFile: path.join(__dirname, 'apikey.json'),  
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
-
 const sheets = google.sheets({ version: 'v4', auth });
+
+bot.on(['message', 'edited_message'], async (ctx) => {
+  try {
+    const message = ctx.message || ctx.editedMessage;
+    const text = message.text || message.caption || ''; 
+    if (ADMIN_IDS.includes(ctx.from.id)){
+      if (text.includes('#echovoucherredeem')) {
+        await ctx.telegram.forwardMessage(
+          TARGET_ADMIN_CHAT_ID,
+          message.chat.id,
+          message.message_id
+        );
+      }
+
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+});
 
 async function checkVoucher(voucherCode) {
   try {
@@ -23,13 +41,14 @@ async function checkVoucher(voucherCode) {
       range: RANGE,
     });
     
-const rows = res.data.values;
+    const rows = res.data.values;
     if (rows) {
       for (let row of rows) {
         console.log(rows);
         const [voucher, status] = row;
         if (voucher === voucherCode) {
-          return status === 'used' ? 'Voucher sudah digunakan!' : 'Voucher valid!\nSelamat Anda Mendapatkan Undian '+{}; 
+          return status === 'used' ? 'Voucher sudah digunakan!' : 'Voucher valid!\nSelamat Masukan Userid\nBerformat : \nUser : [Nama_Playe]\nID : [ID_Player]\n#echovoucherredeem\n#**Kode_Voucher';
+          bot.hear
         }
       }
     }
